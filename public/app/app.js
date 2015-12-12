9925cc1831c54eadcf532dd983f2c589d14e5ee1
Masum Ahammed <masum.ahammed@gmail.com>
2015-12-12 20:18:20 +0600

    remove unnecessary code

--- public/app/app.js
+++ public/app/app.js
@@ -7,9 +7,6 @@
         'ngRoute',          // routing
         'ngSanitize',       // sanitizes html bindings (ex: sidebar.js)
 
-        // Custom modules 
-        'common',           // common functions, logger, spinner
-        'common.bootstrap', // bootstrap dialog wrapper functions
 
         // 3rd Party Modules
         'ui.bootstrap'      // ui-bootstrap (ex: carousel, pagination, dialog)
--- public/app/registration/registration.js
+++ public/app/registration/registration.js
@@ -1,57 +1,10 @@
 ﻿(function () {
     'use strict';
     var controllerId = 'registration';
-    angular.module('br-reg').controller(controllerId, ['common', 'registrationService', registration]);
+    angular.module('br-reg').controller(controllerId, [ registration]);
 
-    function registration(common, registrationService) {
-
-        var getLogFn = common.logger.getLogFn;
-        var log = getLogFn(controllerId);
-        var logError = getLogFn(controllerId,'error');
-
-        var vm = this;
-        vm.registration = {
-            "username" : "masum2@boomtest.com",
-            "firstName" : "Clark",
-            "lastName" : "Kent",
-            "category" : "Super Heros",
-            "businessName" : "Super awesome test",
-
-            "businessDescription" : "Fighting crime",
-
-            "password" : "Test12345",
-            "businessDescription " : "Test test 1",
-            "websiteUrl" : "www.overthere.com",
-            "businessPhoneNumber" : "9545551234",
-            "contactPhoneNumber" : "9545551234"
-        }
+    function registration( ) {
 
 
-
-        vm.title = 'Registration';
-
-        activate();
-
-        function activate() {
-//            var promises = [getMessageCount(), getPeople()];
-//            common.activateController(promises, controllerId)
-//                .then(function () { log('Activated Dashboard View'); });
-        }
-        vm.save = function(){
-            registrationService.saveRegistration(vm.registration).then(function(result){
-                log(result.firstName + ' successfully registered.');
-            },function(err){
-                logError(err);
-            })
-        }
-
-        vm.submitForm = function(isValid) {
-
-            // check to make sure the form is completely valid
-            if (isValid) {
-               vm.save();
-            }
-
-        };
     }
 })();
